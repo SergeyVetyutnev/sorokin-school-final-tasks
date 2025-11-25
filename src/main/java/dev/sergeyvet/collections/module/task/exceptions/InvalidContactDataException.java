9f64bae5d59ec11d88419e25279356e639d30891
SergeyVetyutnev <sergey.vetyutnev@gmail.com>
2025-11-25 15:51:05 +0300

    выполнено финальное задание по коллекциям

---
+++ src/main/java/dev/sergeyvet/collections/module/task/exceptions/InvalidContactDataException.java
@@ -0,0 +1,7 @@
+package dev.sergeyvet.collections.module.task.exceptions;
+
+public class InvalidContactDataException extends RuntimeException {
+    public InvalidContactDataException(String message) {
+        super(message);
+    }
+}
